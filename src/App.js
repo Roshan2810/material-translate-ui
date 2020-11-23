43c1ebe04a48ffa1a20f1406e5f3395479b90d8f
Roshan2810 <rosu285shah@gmail.com>
2020-11-23 16:52:01 +0530

    clear button functionality added

--- src/App.js
+++ src/App.js
@@ -1,49 +1,57 @@
-import React from 'react';
-import { Card, Typography } from '@material-ui/core';
+import React,{Component} from 'react';
+import { Card, Typography} from '@material-ui/core';
 import ButtonList from './components/ButtonList';
 import SelectList from './components/SelectList';
 import MenuBar from './components/MenuBar';
 import './App.css';
 
-function App() {
-  return (
-    <>
-      <MenuBar />
-      <Typography style={{
-       'margin-top': '60px',
-        color: '#0c016e', 'font-family': 'Times New Roman, Times, serif',
-        'text-align': 'center',
-      }}
-        variant="h4">
-        Translate Sentence
-      </Typography>
-      <Card style={{
-        'margin': 'auto',
-        'margin-top': '40px',
-        'padding': '30px',
-        'width': '500px'
-      }}>
-{/* Source Language */}
-        <Typography variant='h6'>
-          Select Source Language
-          <SelectList marginLeft={82}/>
-        </Typography>
+class App extends Component {
 
-        <Typography variant='h6'>
-          Select Target Language
-        <SelectList marginLeft={90}/>
-        </Typography>
+  onClrBtn = ()=>{
+      this.textField.value="";
+  }
 
-        <Typography>
-          <textarea placeholder="Enter Text Here......" style={{ 'border-radius': '5px' }} rows='5' cols="64"></textarea>
-        </Typography>
-        <Typography style={{ margin: '10px' }}>
-          <input type='checkbox'></input>Machine Translated
-        </Typography>
-        <ButtonList />
-      </Card>
-    </>
-  );
+ render(){
+   return (
+     <>
+       <MenuBar />
+       <Typography style={{
+        'margin-top': '60px',
+         color: '#0c016e', 'font-family': 'Times New Roman, Times, serif',
+         'text-align': 'center',
+       }}
+         variant="h4">
+         Translate Sentence
+       </Typography>
+       <Card style={{
+         'margin': 'auto',
+         'margin-top': '40px',
+         'padding': '30px',
+         'width': '500px'
+       }}>
+ {/* Source Language */}
+         <Typography variant='h6'>
+           Select Source Language
+           <SelectList marginLeft={82}/>
+         </Typography>
+ 
+         <Typography variant='h6'>
+           Select Target Language
+         <SelectList marginLeft={90}/>
+         </Typography>
+ 
+
+         <Typography>
+           <textarea ref={e=>this.textField = e} placeholder="Enter Text Here......" style={{ 'border-radius': '5px' }} rows='5' cols="64"></textarea>
+         </Typography>
+         <Typography style={{ margin: '10px' }}>
+           <input type='checkbox'></input>Machine Translated
+         </Typography>
+         <ButtonList onClrBtn={this.onClrBtn}/>
+       </Card>
+     </>
+   );
+ }
 }
 
 export default App;
--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Button} from '@material-ui/core';
 
-const ButtonList = () => {
+const ButtonList = ({onClrBtn}) => {
     return (
-        <><Button style={{ width: '235px', 'margin-left': '6px', 'margin-right': '2px', 'border-radius': '20px', 'font-size': '12px' }} color="primary" variant="contained">Clear</Button>
-            <Button style={{ width: '235px', 'margin-left': '2px', 'border-radius': '20px', 'font-size': '12px' }} color="primary" variant="contained">Submit</Button>
+        <>
+        <Button style={{ width: '235px', 'margin-left': '6px', 'margin-right': '2px', 'border-radius': '20px', 'font-size': '12px' }} color="primary" onClick={onClrBtn} variant="contained">Clear</Button>
+        <Button style={{ width: '235px', 'margin-left': '2px', 'border-radius': '20px', 'font-size': '12px' }} color="primary" variant="contained">Submit</Button>
         </>)
 };
 
